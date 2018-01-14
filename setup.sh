47b17978a05dd8a1a29bac85728452e5ef73b2f4
yesheng701 <563775886@qq.com>
2018-01-14 11:06:29 +0800

    ...

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ redis-server ./conf/redis.conf
 #启动fastdfs - tracker
 fdfs_trackerd ./conf/tracker.conf restart
 #启动fastdfs - storage
-fdfs_storaged /root/workspace/go/src/iHome/conf/storage.conf restart
+fdfs_storaged /root/workspace/go/src/ihome/conf/storage.conf restart
